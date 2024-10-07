d4541f5396cc3927472a01ba83ee23e3213683a9
auwelord <apalancher@gmail.com>
2024-10-07 13:00:59 +0200

    fix: gestion des erreurs api altered

--- controllers/altered.js
+++ controllers/altered.js
@@ -1,53 +1,70 @@
 const axios = require('axios');
-const tools = require('../resources/tools');
 const _ = require('lodash');
 
-exports.g_getCardFromApi = (req, res) => tools.traille(() => getCardFromApi (req, res), res)
-exports.g_getCardsFromApi = (req, res) => tools.traille(() => getCardsFromApi (req, res), res)
-exports.g_getDeckFromApi = (req, res) => tools.traille(() => getDeckFromApi (req, res), res)
+exports.g_getCardFromApi = (req, res) => getCardFromApi (req, res)
+exports.g_getCardsFromApi = (req, res) => getCardsFromApi (req, res)
+exports.g_getDeckFromApi = (req, res) => getDeckFromApi (req, res)
 
 async function getDeckFromApi (req, res)
 {
-    const { data, error} = await axios.get(process.env.ALTERED_DECK_ENDPOINT + req.params.id)
-    
-    if(error)
-        res.status(error.status).send(error);
-    else
-        res.status(200).json(data)
+    try{
+        const { data, error} = await axios.get(process.env.ALTERED_DECK_ENDPOINT + req.params.id)
+        
+        if(error)
+            res.status(error.status).send(error);
+        else
+            res.status(200).json(data)
+    }
+    catch(perror)
+    {
+        res.status(perror.response.status).send(perror.response.data);
+    }
 }
 
 async function getCardFromApi (req, res)
 {
-    const { data, error } = await axios.get(process.env.ALTERED_CARDS_ENDPOINT + req.params.ref,
+    try{
+        const { data, error } = await axios.get(process.env.ALTERED_CARDS_ENDPOINT + req.params.ref,
+        {
+            headers: {"Accept-Language": "fr-fr"},
+            params: {itemsPerPage: 1, page: 1}
+        })
+
+        if(error)
+            res.status(error.status).send(error);
+        else
+            res.status(200).json(data)
+    }
+    catch(perror)
     {
-        headers: {"Accept-Language": "fr-fr"},
-        params: {itemsPerPage: 1, page: 1}
-    })
- 
-    if(error)
-        res.status(error.status).send(error);
-    else
-        res.status(200).json(data)
+        res.status(perror.response.status).send(perror.response.data);
+    }
 }
 
 async function getCardsFromApi (req, res)
 {
-    var headers = {
-        "Accept-Language": "fr-fr"
-    }
+    try{
+        var headers = {
+            "Accept-Language": "fr-fr"
+        }
 
-    if(req.headers.authorization)
-    {
-        headers['Authorization'] = req.headers.authorization
+        if(req.headers.authorization)
+        {
+            headers['Authorization'] = req.headers.authorization
+        }
+        const { data, error } = await axios.get(process.env.ALTERED_CARDS_ENDPOINT,
+        {
+            headers: headers, 
+            params: req.body
+        })
+    
+        if(error)
+            res.status(error.status).send(error);
+        else
+            res.status(200).json(data)
     }
-    const { data, error } = await axios.get(process.env.ALTERED_CARDS_ENDPOINT,
+    catch(perror)
     {
-        headers: headers, 
-        params: req.body
-    })
- 
-    if(error)
-        res.status(error.status).send(error);
-    else
-        res.status(200).json(data)
+        res.status(perror.response.status).send(perror.response.data);
+    }
 }
